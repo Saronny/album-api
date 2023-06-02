01e46d8c1d7add608c52241966d38219b36bc79b
Mike Dudok <1026366@hr.nl>
2023-06-02 22:12:53 +0200

    Added unit test

---
+++ Album.Api.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+
+namespace Album.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void GetGreeting_WithName_ReturnsGreeting()
+    {
+        var greetingService = new GreetingService();
+
+        string greeting = greetingService.GetGreeting("Mike");
+
+        Assert.Equal("Hello Mike", greeting);
+    }
+
+    [Theory]
+    [InlineData(null, "Hello World")]
+    [InlineData("", "Hello World")]
+    [InlineData(" ", "Hello World")]
+
+    public void GetGreeting_WithoutName_ReturnsGreeting(string name, string expectedGreeting)
+    {
+        var greetingService = new GreetingService();
+
+        string greeting = greetingService.GetGreeting(name);
+
+        Assert.Equal(expectedGreeting, greeting);
+    }
+}
